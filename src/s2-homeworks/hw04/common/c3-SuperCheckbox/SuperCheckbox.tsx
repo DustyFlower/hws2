8c70e9988196ef381fc05930f8b286bc5d69b3c5
DustyFlower <lukashevanatalia1992@gmail.com>
2025-09-18 16:31:41 +0200

    hw4 refactoring

--- src/s2-homeworks/hw04/common/c3-SuperCheckbox/SuperCheckbox.tsx
+++ src/s2-homeworks/hw04/common/c3-SuperCheckbox/SuperCheckbox.tsx
@@ -28,6 +28,9 @@ const SuperCheckbox: React.FC<SuperCheckboxPropsType> = (
 ) => {
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
         // задачка на написание онченджа
+        if (onChange) {
+            onChange(e) // это вызовет setChecked(e.currentTarget.checked)
+        }
         if (onChangeChecked) {
             onChangeChecked(e.currentTarget.checked)
         }
